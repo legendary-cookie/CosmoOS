7b77bfa5a5f018923ec2dd072db57b45bc6425b8
legendary-cookie <github.com@xolley.de>
2021-09-08 15:02:29 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
+    loop {}
+}
+
+// in main.rs
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
