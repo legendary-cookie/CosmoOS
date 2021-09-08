08db18c338cdded41bae257609349864cda5e71f
legendary-cookie <github.com@xolley.de>
2021-09-08 19:55:51 +0200

    stop .

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -30,8 +30,10 @@ lazy_static! {
     };
 }
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    /*
     use crate::print;
     print!(".");
+    */
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
