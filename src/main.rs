276fc1a61ee7a247af0ea8b24cad9e605bb4133d
legendary-cookie <github.com@xolley.de>
2021-09-08 15:50:15 +0200

    libsplit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(cosmo_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-#[macro_use]
-mod vga_buffer;
+use cosmo_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
     #[cfg(test)]
     test_main();
+
     loop {}
 }
 
@@ -28,30 +28,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("[failed]\n");
-    println!("Error: {}\n", info);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run(); // new
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        print!("{}...\t", core::any::type_name::<T>());
-        self();
-        println!("[ok]");
-    }
+    cosmo_os::test_panic_handler(info)
 }
